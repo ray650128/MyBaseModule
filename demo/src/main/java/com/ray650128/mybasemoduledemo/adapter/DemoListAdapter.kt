b4d7a7e947cbf82a28d2b097aa4dd19ca4235503
楊昀羲 <ray650128@gmail.com>
2021-07-24 22:14:56 +0800

    Adapter改用ViewBinding

--- demo/src/main/java/com/ray650128/mybasemoduledemo/adapter/DemoListAdapter.kt
+++ demo/src/main/java/com/ray650128/mybasemoduledemo/adapter/DemoListAdapter.kt
@@ -1,11 +1,9 @@
 package com.ray650128.mybasemoduledemo.adapter
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.ray650128.mybasemoduledemo.R
+import com.ray650128.mybasemoduledemo.databinding.ItemDemoListBinding
 import com.ray650128.mybasemoduledemo.model.Student
 
 class DemoListAdapter(data: ArrayList<Student>) : RecyclerView.Adapter<DemoListAdapter.ViewHolder>() {
@@ -17,8 +15,8 @@ class DemoListAdapter(data: ArrayList<Student>) : RecyclerView.Adapter<DemoListA
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DemoListAdapter.ViewHolder {
         val context = parent.context
         val inflater = LayoutInflater.from(context)
-        val view = inflater.inflate(R.layout.item_demo_list, parent, false)
-        return ViewHolder(view)
+        val binding = ItemDemoListBinding.inflate(inflater, parent, false)
+        return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: DemoListAdapter.ViewHolder, position: Int) {
@@ -35,13 +33,10 @@ class DemoListAdapter(data: ArrayList<Student>) : RecyclerView.Adapter<DemoListA
         notifyDataSetChanged()
     }
 
-    inner class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
-        private var studentName: TextView = view.findViewById(R.id.textStudentName)
-        private var studentId: TextView = view.findViewById(R.id.textStudentId)
-
+    inner class ViewHolder(private val binding: ItemDemoListBinding): RecyclerView.ViewHolder(binding.root) {
         fun bindData(student: Student) {
-            studentName.text = student.name
-            studentId.text = "${student.id}"
+            binding.textStudentName.text = student.name
+            binding.textStudentId.text = "${student.id}"
         }
     }
 }
--- demo/src/main/java/com/ray650128/mybasemoduledemo/adapter/StudentScoreAdapter.kt
+++ demo/src/main/java/com/ray650128/mybasemoduledemo/adapter/StudentScoreAdapter.kt
@@ -6,6 +6,7 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.ray650128.mybasemoduledemo.R
+import com.ray650128.mybasemoduledemo.databinding.ItemExamListBinding
 import com.ray650128.mybasemoduledemo.model.Exam
 import com.ray650128.mybasemoduledemo.model.Student
 
@@ -18,8 +19,8 @@ class StudentScoreAdapter(data: ArrayList<Exam>) : RecyclerView.Adapter<StudentS
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StudentScoreAdapter.ViewHolder {
         val context = parent.context
         val inflater = LayoutInflater.from(context)
-        val view = inflater.inflate(R.layout.item_exam_list, parent, false)
-        return ViewHolder(view)
+        val binding = ItemExamListBinding.inflate(inflater, parent, false)
+        return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: StudentScoreAdapter.ViewHolder, position: Int) {
@@ -36,13 +37,10 @@ class StudentScoreAdapter(data: ArrayList<Exam>) : RecyclerView.Adapter<StudentS
         notifyDataSetChanged()
     }
 
-    inner class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
-        private var subjectName: TextView = view.findViewById(R.id.textSubject)
-        private var score: TextView = view.findViewById(R.id.textScore)
-
+    inner class ViewHolder(private val binding: ItemExamListBinding): RecyclerView.ViewHolder(binding.root) {
         fun bindData(exam: Exam) {
-            subjectName.text = exam.subjectName
-            score.text = "${exam.score}"
+            binding.textSubject.text = exam.subjectName
+            binding.textScore.text = "${exam.score}"
         }
     }
 }
