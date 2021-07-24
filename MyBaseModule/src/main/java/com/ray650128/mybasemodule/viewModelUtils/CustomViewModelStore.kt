2b88f87ccb32ddba3838aa36c0fd17819887d7d6
楊昀羲 <ray650128@gmail.com>
2021-07-24 18:51:29 +0800

    補上ViewModel工具的註解

--- MyBaseModule/src/main/java/com/ray650128/mybasemodule/viewModelUtils/CustomViewModelStore.kt
+++ MyBaseModule/src/main/java/com/ray650128/mybasemodule/viewModelUtils/CustomViewModelStore.kt
@@ -1,25 +1,52 @@
 package com.ray650128.mybasemodule.viewModelUtils
 
 import androidx.activity.ComponentActivity
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.*
 
-private val vMStores = HashMap<String, CustomViewModelStore>()
+private val vmStores = HashMap<String, CustomViewModelStore>()
 
-fun ComponentActivity.injectViewModel() {
+/**
+ * 將 ViewModel 與 Activity 綁定
+ */
+fun ComponentActivity.bindViewModel() {
     this::class.java.declaredFields.forEach { field ->
-        field.getAnnotation(ViewModelField::class.java)?.also { scope ->//获取作用域
+        field.getAnnotation(ViewModelField::class.java)?.also { scope -> // 取得作用域
             val element = scope.scopeName
-            var store: CustomViewModelStore
-            if (vMStores.keys.contains(element)) {//如果该作用域存在缓存，则从缓存中获取view model商店
-                store = vMStores[element]!!
-            } else {//如果不存在则创建一个新的商店
+            val store: CustomViewModelStore
+            if (vmStores.keys.contains(element)) { //如果該作用域在快取內，則從快取中取得 ViewModel store
+                store = vmStores[element]!!
+            } else {    // 如果作用域不存在則建立一個新的 ViewModel store
                 store = CustomViewModelStore()
-                vMStores[element] = store
+                vmStores[element] = store
             }
             store.bindHost(this)
             val clazz = field.type as Class<ViewModel>
             val vm = ViewModelProvider(store, VMFactory()).get(clazz)
-            //给view model赋值
+            // ViewModel 給值
+            field.set(this, vm)
+        }
+    }
+}
+
+/**
+ * 將 ViewModel 與 Fragment 綁定
+ */
+fun Fragment.bindViewModel() {
+    this::class.java.declaredFields.forEach { field ->
+        field.getAnnotation(ViewModelField::class.java)?.also { scope -> // 取得作用域
+            val element = scope.scopeName
+            val store: CustomViewModelStore
+            if (vmStores.keys.contains(element)) { //如果該作用域在快取內，則從快取中取得 ViewModel store
+                store = vmStores[element]!!
+            } else {    // 如果作用域不存在則建立一個新的 ViewModel store
+                store = CustomViewModelStore()
+                vmStores[element] = store
+            }
+            store.bindHost(this)
+            val clazz = field.type as Class<ViewModel>
+            val vm = ViewModelProvider(store, VMFactory()).get(clazz)
+            // ViewModel 給值
             field.set(this, vm)
         }
     }
@@ -29,20 +56,19 @@ class CustomViewModelStore : ViewModelStoreOwner {
     private val bindTargets = ArrayList<LifecycleOwner>()
     private var vmStore: ViewModelStore? = null
 
-    //绑定
+    // 綁定
     fun bindHost(host: LifecycleOwner) {
         if (!bindTargets.contains(host)) {
             bindTargets.add(host)
-            //绑定生命周期
+            // 綁定生命週期
             host.lifecycle.addObserver(object : LifecycleEventObserver {
                 override fun onStateChanged(source: LifecycleOwner, event: Lifecycle.Event) {
                     if (event == Lifecycle.Event.ON_DESTROY) {
                         bindTargets.remove(host)
-                        if (bindTargets.isEmpty()) {//如果当前商店没有关联对象，则释放内存
-                            vMStores.entries.find { it.value == this@CustomViewModelStore }?.also {
+                        if (bindTargets.isEmpty()) {    // 如果 ViewModel store 沒有關聯的對象，則釋放資源
+                            vmStores.entries.find { it.value == this@CustomViewModelStore }?.also { map ->
                                 vmStore?.clear()
-                                vMStores.remove(it.key)
-                                //"clear vMStores:${vMStores.size}".p()
+                                vmStores.remove(map.key)
                             }
                         }
                     }
--- MyBaseModule/src/main/java/com/ray650128/mybasemodule/viewModelUtils/ViewModelField.kt
+++ MyBaseModule/src/main/java/com/ray650128/mybasemodule/viewModelUtils/ViewModelField.kt
@@ -1,6 +1,9 @@
 package com.ray650128.mybasemodule.viewModelUtils
 
+/**
+ * 標示 ViewModel 的作用域
+ * @author     Raymond Yang
+ */
 @Retention(AnnotationRetention.RUNTIME)
 @Target(AnnotationTarget.FIELD)
-annotation class ViewModelField(val scopeName: String) {
-}
+annotation class ViewModelField(val scopeName: String)
